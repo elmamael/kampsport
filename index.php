e13b600213e9dfb7dfbe97e6638d903dbc85dc42
Elin <elin.toverud@gmail.com>
2019-03-04 15:25:16 +0100

    dzfcz

---
+++ index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html>
+<head>
+<link rel="stylesheet" href="css/MyStyleKamp.css">
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+	
+	<header>	
+	<div class="navbar">
+		<nav>
+			<ul>				
+				<li class="leftnav"><a href="">Registrering</a></li>
+				<li class="leftnav"><a href="">Timeplan</a></li>
+				<li class="rightnav"><a href="">Konkuranser</a></li>
+				<li class="rightnav"><a href="">Om oss</a></li>
+			</ul>
+		</nav>
+		<div class="logo"><img src="src/Logo.jpg" alt="logo"></div>
+
+	</div>
+	</header>	
+	
+
+	
+</body>
+</html>
