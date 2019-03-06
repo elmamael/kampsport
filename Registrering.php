682f3949924260687707c7098d6d9845c12a7e9f
Mathias Unhjem Øren <2000mathias@gmail.com>
2019-03-06 11:21:43 +0100

    Update Registrering.php

--- Registrering.php
+++ Registrering.php
@@ -37,12 +37,72 @@
 				<tr><td><div><input type="text" name="etternavn" placeholder="Nordmann"><br></div></td></tr>
 				
 				<tr><td><div>Kjønn:</div></td></tr>
-				<tr><td><div><input type="text" name="etternavn" placeholder="Nordmann"><br></div></td></tr>
+				<tr><td><div><select name="kjonn">
+						<option value="0">Mann</option>
+						<option value="1">Kvinne</option>
+				<br></div></td></tr>
 				
-				<tr><td><div><input type="submit" name="leggtil" value="Registrer elev" onclick="window.location.href='HPforside.php'"></div></td></tr>
+				<tr><td><div>Adresse:</div></td></tr>
+				<tr><td><div><input type="text" name="adresse" placeholder="Hundremeterskogen 1"><br></div></td></tr>
+				
+				<tr><td><div>Fødselsdato:</div></td></tr>
+				<tr><td><div><input type="date" name="fodselsdato"><br></div></td></tr>
+				
+				<tr><td><div>Telefonnummer:</div></td></tr>
+				<tr><td><div><input type="text" name="tlf" placeholder="98765432"><br></div></td></tr>
+				
+				<tr><td><div>Belte:</div></td></tr>
+				<tr><td><div><select name="belte">
+						<option value="2">Hvitt</option>
+						<option value="3">Gult</option>
+						<option value="4">Oransje</option>
+						<option value="5">Grønt</option>
+						<option value="6">Blått</option>
+						<option value="7">Brunt</option>
+						<option value="8">Svart</option>
+					
+				<br></div></td></tr>
+				
+				<tr><td><div><input type="submit" name="leggtil" value="Registrer elev" onclick="window.location.href='IndexH.html'"></div></td></tr>
 				
 			</form>
 		</table>
 	</div>
+	
+	
+	<?php
+			if(isset($_POST["leggtil"]))
+			{
+				include'database.php';
+
+
+
+				$fornavn = $_POST["fornavn"];
+				$etternavn = $_POST["etternavn"];
+				$kjonn = $_POST["kjonn"];
+				$adresse = $_POST["adresse"];
+				$fodselsdato = $_POST["fodselsdato"];
+				$tlf = $_POST["tlf"];
+				$belte = $_POST["belte"];
+
+
+				$sql = "INSERT INTO Medlem (fornavn, etternavn, kjonn, adresse, fodselsdato, tlf, belte) VALUES ('$fornavn', '$etternavn', '$kjonn', '$adresse', '$fodselsdato', '$tlf', '$belte');";
+
+				if($connection->query($sql))
+				{
+
+					echo('<div class="lagttil">');
+					echo('<h3 class="skriftlagttil">');
+					echo("Eleven $fornavn $etternavn ble lagt til!");
+					echo('</h3>');
+					echo '</div>';
+				}
+				else
+				{
+					echo("Feil i brukerinput: Insert into elev " . mysqli_error($connection));
+				}
+			}
+
+		?>
 </body>
 </html>
