2868545b6fe329348379fd74bae9a2197362210f
Elin <elin.toverud@gmail.com>
2019-03-06 10:45:31 +0100

    hjkh

--- Registrering.php
+++ Registrering.php
@@ -6,5 +6,34 @@
 </head>
 
 <body>
+	
+	<?php
+	/**
+	*Setup info:
+	*/
+	/*if(isset($_POST["submit"]))*/
+	{
+		$server = "elevweb.akershus-fk.no";
+		$user = "Toel1108";
+		$password = "1234EE";
+		$databaseName = "Toel1108_kampsport";
+
+		$connection = new MySQLi($server, $user, $password, $databaseName);
+		if($connection -> connect_error)
+		{
+			die("Connection failed ".$connection->connect_error);
+		}
+		
+		/*
+		else
+		{
+			echo("Connection Successfull<br>");
+		}
+		
+		$connection->set_charset("utf8");
+		*/
+	}
+		?>
+	
 </body>
 </html>
