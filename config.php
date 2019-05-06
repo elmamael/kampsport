21591f67f07f963e54e7cbcc9a456138a047d059
Mathias Unhjem Øren <2000mathias@gmail.com>
2019-05-06 19:15:01 +0200

    ee
    e

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+	session_start();
+	
+	if (!isset($_SESSION['lang']))
+		$_SESSION['lang'] = "no";
+	else if (isset($_GET['lang']) && $_SESSION['lang'] != $_GET['lang'] && !empty($_GET['lang'])) { 
+		if ($_GET['lang'] == "no")
+			$_SESSION['lang'] = "no";
+		else if ($_GET['lang'] == "en")
+			$_SESSION['lang'] = "en";
+		}
+
+	require_once("Languages/" . $_SESSION['lang'] . ".php");
+	
+?>
+
