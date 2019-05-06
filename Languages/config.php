3b2fba11aa6265fca2f7b902a67e52f3a4de81cc
Mathias Unhjem Øren <2000mathias@gmail.com>
2019-05-06 19:14:56 +0200

    eee

--- Languages/config.php
+++ Languages/config.php
@@ -8,14 +8,8 @@
 			$_SESSION['lang'] = "no";
 		else if ($_GET['lang'] == "en")
 			$_SESSION['lang'] = "en";
-<<<<<<< HEAD:Languages/config.php
-		
-	require_once "../Languages" . $_SESSION['lang'] . ".php";
-=======
 		}
 
-	require_once("Languages/" . $_SESSION['lang'] . ".php");
->>>>>>> 04a256add07d241a5bf3b21e005361c8cfdf4139:config.php
-	
+	require_once "Languages/" . $_SESSION['lang'] . ".php";
 ?>
 
