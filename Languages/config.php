1a21aebad4d354100f6238a5616c3b28f4e714fd
Elin <elin.toverud@gmail.com>
2019-05-06 19:31:45 +0200

    gjhgkjh

--- Languages/config.php
+++ Languages/config.php
@@ -8,20 +8,10 @@
 			$_SESSION['lang'] = "no";
 		else if ($_GET['lang'] == "en")
 			$_SESSION['lang'] = "en";
-<<<<<<< HEAD
-
-		
-	require_once "Languages/" . $_SESSION['lang'] . ".php";
 
 		}
 
-	
-
-	
-=======
-		}
-
 	require_once "Languages/" . $_SESSION['lang'] . ".php";
->>>>>>> 207b9420d36a56c0ee35782533ceea7e6e0c1697
+
 ?>
 
