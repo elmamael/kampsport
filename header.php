a8907c95a70a5ceca77d9daf66f7225331e950dd
Elin <elin.toverud@gmail.com>
2019-05-06 18:44:49 +0200

    hgjhg

--- header.php
+++ header.php
@@ -1,9 +1,13 @@
 <?php
+<<<<<<< HEAD
 	include "Languages/config.php";
+=======
+	include "Languages/config.php"
+>>>>>>> 04a256add07d241a5bf3b21e005361c8cfdf4139
 ?>
 
 <!doctype html>
-<html>
+<html lang="no">
 <head>
 <link rel="stylesheet" href="css/MyStyleKamp.css">
 <meta charset="UTF-8">
